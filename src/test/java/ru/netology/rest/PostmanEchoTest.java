a38d9af98c2a9bb517a780778310166ac1b36b09
BekkerV8 <valkenshtein@gmail.com>
2022-08-09 00:54:06 -0700

    First commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,9 @@
+package ru.netology.rest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class PostmanEchoTest {
+
+
+}
