cbba9b99d29d8b1e4e526c462b90cc20eddc0b09
BekkerV8 <valkenshtein@gmail.com>
2022-08-09 00:58:40 -0700

    added test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -4,6 +4,16 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
-
-
+    @Test
+    public void shouldPostRawTextTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("hello body")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hello body"));
+    }
 }
